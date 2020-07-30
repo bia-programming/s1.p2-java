03e59cbfbcf59cb87d3a3217a2c7609f79f75eab
bia-programming <bia.pasca@yahoo.com>
2020-07-30 15:44:35 +0300

    Problema 2

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int numar=0;
+	    nr(numar);
+    }
+    static void nr(int nr){
+        for(int i=1;i<=100;i++)
+            System.out.println("nr "+i);
+    }
+}
